b565651c6356be5f015f69e5f375b07304a67167
Daniel Hueholt <dmhuehol@ncsu.edu>
2019-08-29 22:26:44 -0400

    Change more saturation equation uses
    The iceSupersatToRH and rhow functions also need to be updated to the new Improved August-Roche-Magnus equation. Documentation is also updated and citations are added.

--- iceSupersatToRH.m
+++ iceSupersatToRH.m
@@ -1,6 +1,12 @@
 function [RH] = iceSupersatToRH(iceSupersat,T)
 %%iceSupersatToRH
-    %Converts ice supersaturation in decimal to RH in percent
+    %Converts ice supersaturation in decimal to RH in percent. Uses the
+    %Improved August-Roche-Magnus saturation vapor pressure equation from:
+    % Alduchov, O.A. and R.E. Eskridge, 1996: 
+    % Improved Magnus Form Approximation of Saturation Vapor Pressure.
+    % J. Appl. Meteor., 35, 601?609,
+    % https://doi.org/10.1175/1520-0450(1996)035<0601:IMFAOS>2.0.CO;2
+    % See equations 25 and 27 from above citation.
     %
     %General form: [RH] = iceSupersatToRH(iceSupersat,T)
     %
@@ -14,22 +20,26 @@ function [RH] = iceSupersatToRH(iceSupersat,T)
     %Written by: Daniel Hueholt
     %North Carolina State University
     %Undergraduate Research Assistant at Environment Analytics
-    %Version date: 4/30/2019
-    %Last major revision: 4/30/2019
+    %Version date: 8/29/2019
+    %Last major revision: 8/29/2019
     %
     %See also rhow
     %
-%Constants
-Lsub = 2.834*10^6; %J/(kg)
-Lvap = 2.501*10^6; %J/Kg
-Rv = 461.5; %J/(kgK)
-es0 = 611; %Pa
+%Constants for MEA312 equation
+% Lsub = 2.834*10^6; %J/(kg)
+% Lvap = 2.501*10^6; %J/Kg
+% Rv = 461.5; %J/(kgK)
+% es0 = 611; %Pa
 
 iceSupersat = iceSupersat./100;
 
-Tk = T+273.15;
-eswStandard = es0*exp(Lvap/Rv*(1/273.15-1./Tk)); %Saturated vapor pressure with respect to water
-esiStandard = es0*exp(Lsub/Rv*(1/273.15-1./Tk)); %Saturated vapor pressure with respect to ice
+eswStandard = 6.1094.*exp((17.625.*T)./(243.04+T));
+esiStandard = 6.1121.*exp((22.587.*T)./(273.86+T));
+
+% Old MEA312 equation
+%Tk = T+273.15;
+%eswStandard = es0*exp(Lvap/Rv*(1/273.15-1./Tk)); %Saturated vapor pressure with respect to water
+%esiStandard = es0*exp(Lsub/Rv*(1/273.15-1./Tk)); %Saturated vapor pressure with respect to ice
 %esiStandard = esiStandard./100;
 
 esw = esiStandard.*(iceSupersat+1);
--- rhow.m
+++ rhow.m
@@ -1,6 +1,11 @@
 function [rhoDiff] = rhow(RHpercent,T)
 %%rhow
-    %Calculates vapor density for a given RH percent and T
+    %Calculates vapor density for a given RH percent and T. Uses the
+    %Improved August-Roche-Magnus saturation vapor pressure equation from:
+    % Alduchov, O.A. and R.E. Eskridge, 1996: 
+    % Improved Magnus Form Approximation of Saturation Vapor Pressure.
+    % J. Appl. Meteor., 35, 601?609,
+    % https://doi.org/10.1175/1520-0450(1996)035<0601:IMFAOS>2.0.CO;2
     %
     %General form: [rhoDiff] = rhow(RHpercent,T)
     %
@@ -18,18 +23,23 @@ function [rhoDiff] = rhow(RHpercent,T)
     %Last major revision: 
     %
 
-%Constants
-Lsub = 2.834*10^6; %J/(kg)
-Lvap = 2.501*10^6; %J/Kg
-Rv = 461.5; %J/(kgK)
-es0 = 611; %Pa
+%Constants originally required by MEA312 saturation equation
+%Lsub = 2.834*10^6; %J/(kg)
+%Lvap = 2.501*10^6; %J/Kg
+%es0 = 611; %Pa
 
 RHdecimal = RHpercent/100; %Equations use decimal instead of percent
-
+Rv = 461.5; %J/(kgK)
 Tk = T+273.15;
-eswStandard = es0*exp(Lvap/Rv*(1/273.15-(1./Tk))); %Saturated vapor pressure with respect to water
-esiStandard = es0*exp(Lsub/Rv*(1/273.15-(1./Tk))); %Saturated vapor pressure with respect to ice
 
+% Original MEA312 equation
+%eswStandard = es0*exp(Lvap/Rv*(1/273.15-(1./Tk))); %Saturated vapor pressure with respect to water
+%esiStandard = es0*exp(Lsub/Rv*(1/273.15-(1./Tk))); %Saturated vapor pressure with respect to ice
+
+eswStandard = 6.1094.*exp((17.625.*T)./(243.04+T));
+esiStandard = 6.1121.*exp((22.587.*T)./(273.86+T));
+eswStandard = eswStandard*100;
+esiStandard = esiStandard*100;
 eswPercent = eswStandard.*RHdecimal;
 
 rhow = eswPercent./(Rv*Tk);
