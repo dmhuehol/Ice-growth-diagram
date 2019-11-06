88927dc9fcb0856e938ebc7e2a30f986fdaf05c6
Daniel Hueholt <dmhuehol@ncsu.edu>
2019-11-05 20:28:08 -0500

    Clean up code
    Documentation and comments are edited and updated. Variable checks are made more effective and error messages are made more understandable. Fixes retaining of Plate field when crystalLog = 0. Removes disabled code that uses old vapor pressure equation.

--- makeGrowthDiagramStruct.m
+++ makeGrowthDiagramStruct.m
@@ -14,7 +14,7 @@ function [hd] = makeGrowthDiagramStruct(crystalLog,otherLog)
     %
     %Output
     %hd: a structure containing name, plot colors, temperature bounds,
-    %   and ice supersaturation bounds for all habits
+    %   RHice bounds, and RHwater bounds for all habits
     %
     %Inputs
     %crystalLog: logical 1/0 whether or not to contain info for crystal
@@ -31,84 +31,86 @@ function [hd] = makeGrowthDiagramStruct(crystalLog,otherLog)
     %See also iceSupersatToRH
     %
 
-%% Variable Checks
-if ~exist('crystalLog','var')
-    disp('Growth info included by default')
-    crystalLog=1;
+%% Variable checks
+if nargin ~= 2
+    msg = 'Requires two inputs! Check function call and try again.';
+    error(msg)
 end
-if ~exist('otherLog','var')
-    disp('Info for other useful parameters included by default')
-    otherLog=1;
+
+if ~isnumeric(crystalLog) || ~isnumeric(otherLog)
+    msg = 'Inputs must be 0 or 1! Check input and try again.';
+    error(msg)
 end
-if crystalLog~=1 && crystalLog~=0
-    msg = 'crystalLog input must be 1 or 0';
-    error(msg);
+
+if crystalLog==1 && otherLog==1
+    disp('Structure includes crystal growth and information for other parameters.')
 end
-if otherLog~=1 && otherLog~=0
-    msg = 'otherLog input must be 1 or 0';
-    error(msg);
+if crystalLog==1 && otherLog==0
+    disp('Structure includes crystal growth information only.')
+end
+if crystalLog==0 && otherLog==1
+    disp('Structure includes non-crystal growth information only.')
+end
+if crystalLog==0 && otherLog==0
+    disp('Empty structure created.')
+    hd = struct([]);
+    return
 end
 
 %% Setup
-hd = struct('Plates',''); %Structure must have at least one field in order to assign elements using dot notation
+hd = struct('Plates',''); %Structure needs at least one field to use dot notation
 
-% Constants
-hd.Constants.Lsub = 2.834*10^6; %J/(kgK)
-hd.Constants.Lvap = 2.501*10^6; %J/(kgK)
-hd.Constants.Rv = 461.5; %J/(kgK)
-hd.Constants.es0 = 611; %Pa
+% Thermodynamic constants
+hd.Constants.Lsub = 2.834*10^6; %J/(kgK) (latent heat of sublimation)
+hd.Constants.Lvap = 2.501*10^6; %J/(kgK) (latent heat of vaporization)
+hd.Constants.Rv = 461.5; %J/(kgK) (ideal gas constant)
+hd.Constants.es0 = 611; %Pa (vapor pressure constant)
 
-%% Make the structure
+%% Make structure
 if crystalLog==1
     
     % "Various plates" habit
     T_vp = [-8 -22];
-    %T_vp = T_vp + 273.15; %MEA312 saturation equation needed T to be in K
     esw_vp = 6.1094.*exp((17.625.*T_vp)./(243.04+T_vp));
     esi_vp = 6.1121.*exp((22.587.*T_vp)./(273.86+T_vp));
 
-    %esw_vp = hd.Constants.es0*exp(hd.Constants.Lvap/hd.Constants.Rv*(1/273.15-1./T_vp)); %Original MEA312 saturation equation
-    %esi_vp = hd.Constants.es0*exp(hd.Constants.Lsub/hd.Constants.Rv*(1/273.15-1./T_vp)); %Original MEA312 saturation equation
     % "Sector plates" habit
     T_sp = [-8 -12.2; -12.2 -17.6; -17.6 -22];
-    %T_sp = T_sp + 273.15; %MEA312 saturation equation needed T to be in K
     esw_sp = 6.1094.*exp((17.625.*T_sp)./(243.04+T_sp));
     esi_sp = 6.1121.*exp((22.587.*T_sp)./(273.86+T_sp));
-    %esw_sp = hd.Constants.es0*exp(hd.Constants.Lvap/hd.Constants.Rv*(1/273.15-1./T_sp)); %Original MEA312 saturation equation
-    %esi_sp = hd.Constants.es0*exp(hd.Constants.Lsub/hd.Constants.Rv*(1/273.15-1./T_sp)); %Original MEA312 saturation equation
     
-    hd.Plates.Habit = 'Edge growth (plate-like)'; %'Plates';
+    hd.Plates.Habit = 'Edge growth (plate-like)'; %Plates
     hd.Plates.Color = [243 139 156]./255;
     hd.Plates.TempBounds = [0 0 -4 -4];
     hd.Plates.supersatBounds = [0 0.1 0.1 0];
     hd.Plates.waterBounds = iceSupersatToRH(hd.Plates.supersatBounds.*100,hd.Plates.TempBounds);
 
-    hd.ColumnLike.Habit = 'Face growth (column-like)'; %'Column-like';
+    hd.ColumnLike.Habit = 'Face growth (column-like)'; %Column-like
     hd.ColumnLike.Color = [165 162 221]./255;
     hd.ColumnLike.TempBounds = [-4 -4 -8 -8];
     hd.ColumnLike.supersatBounds = [0 0.28 0.28 0];
     hd.ColumnLike.waterBounds = iceSupersatToRH(hd.ColumnLike.supersatBounds.*100,hd.ColumnLike.TempBounds);
     
-    hd.VariousPlates.Habit = 'Edge growth (various plates)'; %'Various plates';
+    hd.VariousPlates.Habit = 'Edge growth (various plates)'; %Various plates
     hd.VariousPlates.Color = hd.Plates.Color;
     hd.VariousPlates.TempBounds = [-8 -8 -22 -22];
     hd.VariousPlates.supersatBounds = [0 (esw_vp(1)-esi_vp(1))/esi_vp(1) (esw_vp(2)-esi_vp(2))/esi_vp(2) 0];
     hd.VariousPlates.waterBounds = iceSupersatToRH(hd.VariousPlates.supersatBounds.*100,hd.VariousPlates.TempBounds);
     
-    hd.SectorPlates.Habit = 'Corner growth (sector plates)'; %'Sector plates';
+    hd.SectorPlates.Habit = 'Corner growth (sector plates)'; %Sector plates
     hd.SectorPlates.Color =  [218 146 56]./255;
     hd.SectorPlates.TempBounds = [-8 -8 -12.2 -12.2; -12.2 -12.2 -17.6 -17.6; -17.6 -17.6 -22 -22];
     hd.SectorPlates.supersatBounds = [(esw_sp(1)-esi_sp(1))/esi_sp(1) 0.36 0.36 (esw_sp(4)-esi_sp(4))/esi_sp(4); (esw_sp(2)-esi_sp(2))/esi_sp(2) 0.15 0.21 (esw_sp(5)-esi_sp(5))/esi_sp(5); (esw_sp(3)-esi_sp(3))/esi_sp(3) 0.6 0.6 (esw_sp(6)-esi_sp(6))/esi_sp(6)];
     hd.SectorPlates.waterBounds = iceSupersatToRH(hd.SectorPlates.supersatBounds.*100,hd.SectorPlates.TempBounds);
     
-    hd.Dendrites.Habit = 'Corner growth (branched, dendrites)'; %'Dendrites';
+    hd.Dendrites.Habit = 'Corner growth (branched, dendrites)'; %Dendrites
     hd.Dendrites.Color = [247 214 153]./255;
     hd.Dendrites.TempBounds = [-12.7 -12.7 -17.1 -17.1];
     hd.Dendrites.supersatBounds = [0.16 0.43 0.43 0.21];
     hd.Dendrites.waterBounds = iceSupersatToRH(hd.Dendrites.supersatBounds.*100,hd.Dendrites.TempBounds);
     
     hd.PolycrystalsP.Habit = 'Polycrystals (platelike)';
-    hd.PolycrystalsP.Color = [89 25 42]./255; %[63 1 22]./255;
+    hd.PolycrystalsP.Color = [89 25 42]./255;
     hd.PolycrystalsP.TempBounds = [-46.6 -40.2 -22 -22; -40.2 -40.2 -22 -22];
     hd.PolycrystalsP.supersatBounds = [0.038 0.33 0.33 0.038; 0.33 0.6 0.6 0.33];
     hd.PolycrystalsP.waterBounds = iceSupersatToRH(hd.PolycrystalsP.supersatBounds.*100,hd.PolycrystalsP.TempBounds);
@@ -125,12 +127,6 @@ if crystalLog==1
     hd.Mixed.supersatBounds = [0 0.038 0.038 0; 0.038 0.0697 0.0697 0.038];
     hd.Mixed.waterBounds = iceSupersatToRH(hd.Mixed.supersatBounds.*100,hd.Mixed.TempBounds);
     
-    hd.PolycrystalsIntermediate.Habit = 'Possible extension of the bullet rosette habit';
-    hd.PolycrystalsIntermediate.Color = [hd.PolycrystalsP.Color; hd.PolycrystalsP.Color]; %Not shown on current diagram
-    hd.PolycrystalsIntermediate.TempBounds = [-40.2 -40.2 -36.5 -36.5];
-    hd.PolycrystalsIntermediate.supersatBounds = [0.359 0.491 0.445 0.316];
-    hd.PolycrystalsIntermediate.waterBounds = iceSupersatToRH(hd.PolycrystalsIntermediate.supersatBounds.*100,hd.PolycrystalsIntermediate.TempBounds);
-    
 end
 
 if otherLog==1
@@ -141,7 +137,6 @@ if otherLog==1
     TlineStandardC = Tupper:-0.1:Tlower;
     [eswLineData] = eswLine(100,Tlower,Tupper);
     hd.unnatural.TempBounds = [0 TlineStandardC(2:11:420) 0];
-    %hd.unnatural.TempBounds = [0 -7.5 -14 -18.5 -24.8 0]; %Original values from MEA312 saturation equation
     hd.unnatural.supersatBounds = [0 eswLineData(2:11:420).*2 0.6];
     hd.unnatural.waterBounds = iceSupersatToRH(hd.unnatural.supersatBounds.*100,hd.unnatural.TempBounds);
     
@@ -152,11 +147,15 @@ if otherLog==1
     hd.subsaturated.waterBounds = iceSupersatToRH(hd.subsaturated.supersatBounds.*100,hd.subsaturated.TempBounds);
     
     hd.warm.Habit = 'Coordinates to cover the area warmer than freezing (for radiosonde data plotting).';
-    hd.warm.Color = [255,255,255]./255; %[204 204 204]./255;
+    hd.warm.Color = [255,255,255]./255; %Invisible by default; suggest [204 204 204]./255 if want to plot
     hd.warm.TempBounds = [25 25 0 0];
     hd.warm.supersatBounds = [0 0.6 0.6 0];
     hd.warm.waterBounds = iceSupersatToRH(hd.warm.supersatBounds.*100,hd.warm.TempBounds);
     
+end
+
+if crystalLog == 0
+    hd = rmfield(hd,'Plates'); %Remove plate field if building structure without crystal growth info
 end
 
 end
