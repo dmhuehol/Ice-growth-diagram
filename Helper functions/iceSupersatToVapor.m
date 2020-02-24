4a495b1b527d4346d7143433f2b5cf803e2c8381
Daniel Hueholt <dmhuehol@ncsu.edu>
2020-02-23 20:45:05 -0500

    Line break

--- Helper functions/iceSupersatToVapor.m
+++ Helper functions/iceSupersatToVapor.m
@@ -29,4 +29,5 @@ function [vaporPressure] = iceSupersatToVapor(iceSupersat,temp)
 
 esiStandard = 6.1121.*exp((22.587.*temp)./(273.86+temp)); % Saturation vapor pressure at input temperature
 vaporPressure = iceSupersat.*esiStandard;
+
 end
