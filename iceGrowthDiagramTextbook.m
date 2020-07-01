60342ed199e53ac722fb136a91568e3d98960aeb
Daniel Hueholt <dmhuehol@ncsu.edu>
2020-07-01 14:39:33 -0400

    Add RHw version
    Adds RHw version, same as diagram produced by iceGrowthDiagramWater

--- iceGrowthDiagramTextbook.m
+++ iceGrowthDiagramTextbook.m
@@ -18,27 +18,23 @@
 %Written by: Daniel Hueholt
 %North Carolina State University
 %Research Assistant at Environment Analytics
-%Version date: 6/30/2020
-%Last major revision: 
+%Version date: 7/1/2020
+%Last major revision: 7/1/2020
 %
-%Based on edits to ice growth diagram originally made in Adobe Illustrator
-%by Dr. Matthew Miller, Senior Research Scholar at Environment Analytics.
+%Based on concept art originally made in Adobe Illustrator by Dr. Matthew
+%Miller, Senior Research Scholar at Environment Analytics.
 %
 
 %% Universal
 [hd] = makeGrowthDiagramStruct(1,1);
-
-%% Ice growth diagram in terms of relative humidity with respect to water
-%function [fig,legendEntries,legendTexts] = iceGrowthDiagram(hd,isohumeFlag,ventLog,updraftLog,legLog,legendLocStr,xlimRange,ylimRange)
-
-% Check variables
-xlimRange = [0 0.6];
-ylimRange = [-56.5 0];
-
-% Make s-T diagram
 fig = figure;
 leftColor = [0 0 0]; rightColor = [0 0 0];
 set(fig,'defaultAxesColorOrder',[leftColor; rightColor]) %Sets left and right y-axis color
+
+%% Ice growth diagram in terms of relative humidity with respect to ice
+% Check variables
+xlimRange = [0 0.6];
+ylimRange = [-56.5 0];
 
 % Draw the growth types
 plates = patch(hd.Plates.supersatBounds, hd.Plates.TempBounds,hd.Plates.TextbookColor);
@@ -83,7 +79,7 @@ unnatural = patch(hd.unnatural.supersatBounds,hd.unnatural.TempBounds,hd.unnatur
 unnatural.EdgeColor = 'none';
 hold on
 
-%% Plot other lines
+% Plot isohumes and ventilation lines
 Tupper = 15; Tlower = -70;
 TlineStandardC = Tupper:-0.1:Tlower;
 [eswLineData] = eswLine(100,Tlower,Tupper);
@@ -210,7 +206,7 @@ lVent = text(0.36,-15.9,'Approx. max natural supersat (with ventilation)');
 lVent.FontName = 'Lato'; lVent.FontSize = 12;
 lVent.Rotation = 16;
 
-%% Diagram settings
+% Diagram settings
 axe = gca;
 axe.FontName = 'Lato';
 axe.FontSize = 18;
@@ -241,3 +237,197 @@ xTickLabels = {'100' '105' '110' '115' '120' '125' '130' '135' '140' '145' '150'
 xticklabels(xTickLabels);
 axe.Layer = 'top'; %Forces tick marks to be displayed over the patch objects
 axe.YDir = 'reverse';
+
+%% Ice growth diagram in terms of relative humidity with respect to water
+legLog = 1;
+legendLocStr = 'southoutside';
+
+% Modifiable variables
+xlimRange = [55 124];
+ylimRange = [-56.5 0];
+
+% Draw the growth modes
+Tupper = 15; Tlower = -70;
+TlineStandardC = Tupper:-0.1:Tlower;
+
+plates = patch(hd.Plates.waterBounds, hd.Plates.TempBounds, hd.Plates.TextbookColor);
+plates.EdgeColor = 'none';
+columnlike = patch(hd.ColumnLike.waterBounds,hd.ColumnLike.TempBounds,hd.ColumnLike.TextbookColor);
+columnlike.EdgeColor = 'none';
+variousplates = patch(hd.VariousPlates.waterBounds,hd.VariousPlates.TempBounds,hd.VariousPlates.TextbookColor);
+variousplates.EdgeColor = 'none';
+polycrystalsP1 = patch(hd.PolycrystalsP.waterBounds(1,:),hd.PolycrystalsP.TempBounds(1,:),hd.PolycrystalsP.TextbookColor);
+polycrystalsP1.EdgeColor = 'none';
+polycrystalsP2 = patch(hd.PolycrystalsP.waterBounds(2,:),hd.PolycrystalsP.TempBounds(2,:),hd.PolycrystalsP.TextbookColor);
+polycrystalsP2.EdgeColor = 'none';
+polycrystalsC1 = patch(hd.PolycrystalsC.waterBounds(1,:),hd.PolycrystalsC.TempBounds(1,:),hd.PolycrystalsC.TextbookColor);
+polycrystalsC1.EdgeColor = 'none';
+polycrystalsC2 = patch(hd.PolycrystalsC.waterBounds(2,:),hd.PolycrystalsC.TempBounds(2,:),hd.PolycrystalsC.TextbookColor);
+polycrystalsC2.EdgeColor = 'none';
+sectorplates1 = patch(hd.SectorPlates.waterBounds(1,:),hd.SectorPlates.TempBounds(1,:),hd.SectorPlates.TextbookColor);
+sectorplates1.EdgeColor = 'none';
+sectorplates2 = patch(hd.SectorPlates.waterBounds(2,:),hd.SectorPlates.TempBounds(2,:),hd.SectorPlates.TextbookColor);
+sectorplates2.EdgeColor = 'none';
+sectorplates3 = patch(hd.SectorPlates.waterBounds(3,:),hd.SectorPlates.TempBounds(3,:),hd.SectorPlates.TextbookColor);
+sectorplates3.EdgeColor = 'none';
+dendrites = patch(hd.Dendrites.waterBounds,hd.Dendrites.TempBounds,hd.Dendrites.TextbookColor);
+dendrites.EdgeColor = 'none';
+
+intermediateSPD_floor = patch([hd.Dendrites.waterBounds(1),hd.Dendrites.waterBounds(1) hd.Dendrites.waterBounds(2) hd.Dendrites.waterBounds(2)], [hd.SectorPlates.TempBounds(5) hd.Dendrites.TempBounds(2) hd.Dendrites.TempBounds(2),hd.SectorPlates.TempBounds(5)],reshape([hd.SectorPlates.TextbookColor; hd.Dendrites.TextbookColor; hd.Dendrites.TextbookColor; hd.SectorPlates.TextbookColor],4,[],3));
+intermediateSPD_floor.EdgeColor = 'none';
+intermediateSPD_wall = patch([hd.SectorPlates.waterBounds(2,3) hd.SectorPlates.waterBounds(2,2) hd.Dendrites.waterBounds(1) hd.Dendrites.waterBounds(4)], [hd.SectorPlates.TempBounds(3) hd.SectorPlates.TempBounds(2) hd.Dendrites.TempBounds(1) hd.Dendrites.TempBounds(3)],reshape([hd.SectorPlates.TextbookColor; hd.SectorPlates.TextbookColor; hd.Dendrites.TextbookColor; hd.Dendrites.TextbookColor],4,[],3));
+intermediateSPD_wall.EdgeColor = 'none';
+intermediateSPD_ceiling = patch([hd.Dendrites.waterBounds(4),hd.Dendrites.waterBounds(4) hd.SectorPlates.waterBounds(6) hd.SectorPlates.waterBounds(9)], [hd.Dendrites.TempBounds(4) hd.SectorPlates.TempBounds(11) hd.SectorPlates.TempBounds(11),hd.Dendrites.TempBounds(4)],reshape([hd.Dendrites.TextbookColor; hd.SectorPlates.TextbookColor; hd.SectorPlates.TextbookColor; hd.Dendrites.TextbookColor],4,[],3));
+intermediateSPD_ceiling.EdgeColor = 'none';
+intermediateSPD_triangleTop = patch([hd.SectorPlates.waterBounds(2,3) hd.Dendrites.waterBounds(4) hd.Dendrites.waterBounds(1)], [hd.SectorPlates.TempBounds(3) hd.Dendrites.TempBounds(3) hd.SectorPlates.TempBounds(3)], reshape([hd.SectorPlates.TextbookColor; hd.Dendrites.TextbookColor; hd.SectorPlates.TextbookColor],3,[],3));
+intermediateSPD_triangleTop.EdgeColor = 'none';
+intermediateSPD_triangleBottom = patch([hd.SectorPlates.waterBounds(2,2), hd.Dendrites.waterBounds(1), hd.Dendrites.waterBounds(1)], [hd.SectorPlates.TempBounds(2), hd.Dendrites.TempBounds(1), hd.SectorPlates.TempBounds(2)],reshape([hd.SectorPlates.TextbookColor; hd.Dendrites.TextbookColor; hd.SectorPlates.TextbookColor],3,[],3));
+intermediateSPD_triangleBottom.EdgeColor = 'none';
+
+mixed1 = patch(hd.Mixed.waterBounds(1,:),hd.Mixed.TempBounds(1,:),hd.Mixed.TextbookColor);
+mixed1.EdgeColor = 'none';
+
+mixed2 = patch(hd.Mixed.waterBounds(2,:),hd.Mixed.TempBounds(2,:),hd.Mixed.TextbookColor);
+mixed2.EdgeColor = 'none';
+warmerThanFreezing = patch(hd.warm.waterBounds(1,:),hd.warm.TempBounds(1,:),hd.warm.Color);
+warmerThanFreezing.EdgeColor = 'none';
+subsaturated = patch(hd.subsaturated.waterBounds,hd.subsaturated.TempBounds,hd.subsaturated.Color);
+subsaturated.EdgeColor = 'none';
+unnatural = patch(hd.unnatural.waterBounds,hd.unnatural.TempBounds,hd.unnatural.Color);
+unnatural.EdgeColor = 'none';
+hold on
+
+legendEntries = [plates columnlike sectorplates1 dendrites polycrystalsP1 polycrystalsC1 mixed1 subsaturated];
+legendTexts = {hd.Plates.Habit,hd.ColumnLike.Habit,hd.SectorPlates.Habit,hd.Dendrites.Habit,hd.PolycrystalsP.Habit,hd.PolycrystalsC.Habit,hd.Mixed.Habit,'Subsaturated wrt ice, no crystal growth'};
+
+% Plot other lines
+esw50LineStandard = plot([50 50],[-70 0]);
+esw50LineStandard.Color = [255 230 0]./255;
+esw50LineStandard.LineWidth = 3.2;
+esw50LineStandard.LineStyle = ':';
+
+esw60LineStandard = plot([60 60],[-70 0]);
+esw60LineStandard.Color = [255 230 0]./255;
+esw60LineStandard.LineWidth = 3.2;
+esw60LineStandard.LineStyle = ':';
+
+esw70LineStandard = plot([70 70],[-70 0]);
+esw70LineStandard.Color = [255 230 0]./255;
+esw70LineStandard.LineWidth = 3.2;
+esw70LineStandard.LineStyle = ':';
+
+esw80LineStandard = plot([80 80],[-70 0]);
+esw80LineStandard.Color = [255 230 0]./255;
+esw80LineStandard.LineWidth = 3.2;
+esw80LineStandard.LineStyle = ':';
+
+esw90LineStandard = plot([90 90],[-70 0]);
+esw90LineStandard.Color = [255 230 0]./255;
+esw90LineStandard.LineWidth = 3.2;
+esw90LineStandard.LineStyle = ':';
+
+esw100SupersatLineStandard = plot([100 100],[-70 0]);
+esw100SupersatLineStandard.Color = [255 230 0]./255;
+esw100SupersatLineStandard.LineWidth = 3.2;
+esw100SupersatLineStandard.LineStyle = ':';
+legendEntries(end+1) = esw100SupersatLineStandard;
+legendTexts{end+1} = 'Isohumes (10% intervals)';
+
+esw102p5LineStandard = plot([102.5 102.5],[-70 -2.6]);
+esw102p5LineStandard.Color = [255 230 0]./255;
+esw102p5LineStandard.LineWidth = 3.2;
+esw102p5LineStandard.LineStyle = '-.';
+
+esw105LineStandard = plot([105 105],[-70 -5.2]);
+esw105LineStandard.Color = [255 230 0]./255;
+esw105LineStandard.LineWidth = 3.2;
+esw105LineStandard.LineStyle = '-.';
+legendEntries(end+1) = esw105LineStandard;
+legendTexts{end+1} = 'Isohumes (102.5%, 105%)';
+
+startMat = ones(1,length(TlineStandardC));
+
+water_esi0LineData = iceSupersatToRH(startMat.*0,TlineStandardC);
+esi0Line = plot(water_esi0LineData,TlineStandardC);
+esi0Line.Color = [255 230 0]./255;
+esi0Line.LineWidth = 3.2;
+hold on
+
+water_esi10LineData = iceSupersatToRH(startMat.*10,TlineStandardC);
+esi10Line = plot(water_esi10LineData(201:end),TlineStandardC(201:end));
+esi10Line.Color = [255 230 0]./255;
+esi10Line.LineWidth = 3.2;
+
+water_esi20LineData = iceSupersatToRH(startMat.*20,TlineStandardC);
+esi20Line = plot(water_esi20LineData(248:end),TlineStandardC(248:end));
+esi20Line.Color = [255 230 0]./255;
+esi20Line.LineWidth = 3.2;
+
+water_esi30LineData = iceSupersatToRH(startMat.*30,TlineStandardC);
+esi30Line = plot(water_esi30LineData(291:end),TlineStandardC(291:end));
+esi30Line.Color = [255 230 0]./255;
+esi30Line.LineWidth = 3.2;
+
+water_esi40LineData = iceSupersatToRH(startMat.*40,TlineStandardC);
+esi40Line = plot(water_esi40LineData(335:end),TlineStandardC(335:end));
+esi40Line.Color = [255 230 0]./255;
+esi40Line.LineWidth = 3.2;
+
+water_esi50LineData = iceSupersatToRH(startMat.*50,TlineStandardC);
+esi50Line = plot(water_esi50LineData(376:end),TlineStandardC(376:end));
+esi50Line.Color = [255 230 0]./255;
+esi50Line.LineWidth = 3.2;
+
+water_esi60LineData = iceSupersatToRH(startMat.*60,TlineStandardC);
+esi60Line = plot(water_esi60LineData(416:end),TlineStandardC(416:end));
+esi60Line.Color = [255 230 0]./255;
+esi60Line.LineWidth = 3.2;
+legendEntries(end+1) = esi60Line;
+legendTexts{end+1} = 'Ice-isohumes (100% min, 160% max, 10% interval)';
+
+[eswLineData] = eswLine(100,Tlower,Tupper);
+water_ventLineData = iceSupersatToRH(2*eswLineData(151:end).*100,TlineStandardC(151:end));
+water_ventLine = plot(water_ventLineData,TlineStandardC(151:end));
+water_ventLine.Color = [0 26 255]./255;
+water_ventLine.LineWidth = 3.2;
+legendEntries(end+1) = water_ventLine;
+legendTexts{end+1} = 'Approximate max natural saturation with ventilation';
+
+% Diagram settings
+axe = gca;
+axe.FontName = 'Lato';
+axe.FontSize = 18;
+
+yyaxis right
+[zLabels, tempsInRange, rightLimits] = ylimitsForIceDiagram(ylimRange);
+axe.YTick = tempsInRange;
+yticklabels(zLabels);
+ylim(rightLimits)
+axe.Layer = 'top';
+yLab = ylabel(['Height above 0' char(176) 'C level in m (ICAO standard atmosphere)']);
+yLab.FontName = 'Lato Bold'; yLab.Color = 'k';
+
+yyaxis left %Changes what axis dot notation refers
+ylim(ylimRange)
+xlim(xlimRange)
+
+t = title('Ice growth diagram');
+t.FontName = 'Lato Bold';
+t.FontSize = 20;
+yLab = ylabel(['Temperature in ' char(176) 'C']);
+yLab.FontName = 'Lato Bold';
+xLab = xlabel('Relative humidity with respect to water (%)');
+xLab.FontName = 'Lato Bold';
+axe.YTick = [-70 -60 -55 -50 -40 -30 -22 -20 -18 -16 -14 -12 -10 -8 -6 -4 -2 0 2 4 6 8 10 12];
+axe.XTick = [50 55 60 70 80 90 100 110 120 130 140 150 160 170];
+axe.Layer = 'top'; %Forces tick marks to be displayed over the patch objects
+axe.YDir = 'reverse';
+
+leg = legend(legendEntries,legendTexts);
+leg.Location = legendLocStr;
+leg.NumColumns = 3;
+leg.FontSize = 14;
+
+if legLog==0
+    leg.Visible = 'off';
+end
